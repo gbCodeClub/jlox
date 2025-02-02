d4d35a6f8412bb720c42f64479809e77ed3bc8ab
WHJang-0421 <morrranii@gmail.com>
2025-02-02 07:29:54 +0000

    Chapter 7 - Evaluating Expressions

---
+++ src/main/java/com/gbcodeclub/jlox/Interpreter.java
@@ -0,0 +1,130 @@
+package com.gbcodeclub.jlox;
+
+import com.gbcodeclub.jlox.Expr.Binary;
+import com.gbcodeclub.jlox.Expr.Grouping;
+import com.gbcodeclub.jlox.Expr.Literal;
+import com.gbcodeclub.jlox.Expr.Unary;
+
+public class Interpreter implements Expr.Visitor<Object> {
+
+    void interpret(Expr expression) {
+        try {
+            Object value = evaluate(expression);
+            System.out.println(stringify(value));
+        } catch (RuntimeError error) {
+            Lox.runtimeError(error);
+        }
+    }
+
+    private Object evaluate(Expr expr) {
+        return expr.accept(this);
+    }
+
+    @SuppressWarnings("incomplete-switch")
+    @Override
+    public Object visitBinaryExpr(Binary expr) {
+        Object left = evaluate(expr.left);
+        Object right = evaluate(expr.right);
+
+        switch (expr.operator.type) {
+        case BANG_EQUAL:
+            return !isEqual(left, right);
+        case EQUAL:
+            return isEqual(left, right);
+        case GREATER:
+            checkNumberOperands(expr.operator, left, right);
+            return (double) left > (double) right;
+        case GREATER_EQUAL:
+            checkNumberOperands(expr.operator, left, right);
+            return (double) left >= (double) right;
+        case LESS:
+            checkNumberOperands(expr.operator, left, right);
+            return (double) left < (double) right;
+        case LESS_EQUAL:
+            checkNumberOperands(expr.operator, left, right);
+            return (double) left <= (double) right;
+        case MINUS:
+            checkNumberOperands(expr.operator, left, right);
+            return (double)left - (double)right;
+        case SLASH:
+            checkNumberOperands(expr.operator, left, right);
+            return (double)left / (double)right;
+        case STAR:
+            checkNumberOperands(expr.operator, left, right);
+            return (double)left * (double)right;
+        case PLUS:
+            if (left instanceof Double && right instanceof Double) {
+                return (double)left + (double)right;
+            }
+            if (left instanceof String && right instanceof String) {
+                return (String)left + (String)right;
+            }
+            throw new RuntimeError(expr.operator, "Operands must be 2 numbers or 2 strings");
+        }
+
+        // Unreachable.
+        return null;
+    }
+
+    @Override
+    public Object visitGroupingExpr(Grouping expr) {
+        return evaluate(expr.expression);
+    }
+
+    @Override
+    public Object visitLiteralExpr(Literal expr) {
+        return expr.value;
+    }
+
+    @SuppressWarnings("incomplete-switch")
+    @Override
+    public Object visitUnaryExpr(Unary expr) {
+        Object right = evaluate(expr.right);
+        switch(expr.operator.type) {
+            case MINUS:
+                checkNumberOperand(expr.operator, right);
+                return -(double)right;
+            case BANG:
+                return !isTruthy(right);
+        }
+
+        // Unreachable.
+        return null;
+    }
+
+    private void checkNumberOperand(Token operator, Object operand) {
+        if (operand instanceof Double) return;
+        throw new RuntimeError(operator, "operand must be a number.");
+    }
+
+    private void checkNumberOperands(Token operator, Object left, Object right) {
+        if (left instanceof Double && right instanceof Double) return;
+        throw new RuntimeError(operator, "operands must be numbers.");
+    }
+
+    private boolean isTruthy(Object object) {
+        if (object == null) return false;
+        if (object instanceof Boolean) return (boolean) object;
+        return true;
+    }
+    
+    private boolean isEqual(Object a, Object b) {
+        if (a == null) return b == null;
+
+        return a.equals(b);
+    }
+
+    private String stringify(Object object) {
+        if (object == null) return "nil";
+
+        if (object instanceof Double) {
+            String text = object.toString();
+            if (text.endsWith(".0")) {
+                text = text.substring(0, text.length() - 2);
+            }
+            return text;
+        }
+
+        return object.toString();
+    }
+}
